413773645538e6755074d5f1d220fcfa68b658df
Vijayabalaji B <jayb2495@gmail.com>
2020-06-13 19:00:20 +0530

    committing to access in laptop

--- application.py
+++ application.py
@@ -1,13 +1,52 @@
 import os
 
-from flask import Flask
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_socketio import SocketIO, emit
+from functools import wraps
+from helpers import apology, name_required
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+messages = {}
 
 @app.route("/")
+@name_required
 def index():
-    return "Project 2: TODO"
+    if session["user_id"] == None:
+        return redirect("/getName")
+    else:
+        return render_template("index.html")
+
+@app.route("/getName", methods=["GET", "POST"])
+def userName():
+    session.clear()
+
+    if request.method == "POST":
+        
+        if not request.form.get("username"):
+            return apology("Please type your name", 403)
+
+        session["user_id"] = request.form.get("username")
+
+        return redirect("/")
+
+    else:
+        return render_template("getName.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
